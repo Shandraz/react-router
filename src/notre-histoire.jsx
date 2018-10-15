dd6fc9aa3d0b0726a314835cf9e43447230724d0
Ysatis Mephisto <ysatis@MacBook-Air-de-Ysatis.local>
2018-10-15 14:32:52 +0200

    initial commit

---
+++ src/notre-histoire.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+class History extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Notre histoire</h1>
+                
+                
+            </div>
+        );
+    }
+
+}
+
+export default History;
